217ab4bdec019e87e87c2c32caabf573a4009401
Elijah Frederickson <elijah.frederickson@gmail.com>
2015-04-28 16:03:26 -0800

    Initial Commit

---
+++ OpenInApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenInApp
+//
+//  Created by Elijah Frederickson on 4/28/15.
+//  Copyright (c) 2015 Elijah Frederickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenInApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenInApp
+//
+//  Created by Elijah Frederickson on 4/28/15.
+//  Copyright (c) 2015 Elijah Frederickson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
